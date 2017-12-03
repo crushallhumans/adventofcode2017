d38f9c361c1d852baca07f576d871ffb3cc8b12d
Conrad Rushing <conrad.rushing@gmail.com>
2017-12-03 16:00:35 -0500

    Oh god such ugly code

--- day03.py
+++ day03.py
@@ -16,7 +16,7 @@ def find_square_size(i):
         max_square = square_size ** 2
     return square_size
 
-def center_steps(i, find_prior_square_neighbors = False):
+def center_steps(i):
     print "center_steps for: {}".format(i)
     if i == 1: return 0
 
@@ -56,10 +56,6 @@ def center_steps(i, find_prior_square_neighbors = False):
     ret = int(horizontal_steps + vertical_steps)
     print "steps: {}".format(ret)
 
-    # solve problem 2 - HACK FOR SPECIFIC PUZZLE VAL
-    if find_prior_square_neighbors:
-        # whoo, can't think this one out rn        
-
     return ret
 
 #0 1 2 3
@@ -81,5 +77,282 @@ for t in test_set:
     print '-----'
     assert (t[1] == center_steps(t[0]))
 
+
+print '-----'
+print '-----'
+
+def calculate_neighbors(square, steps, previous_arr, current_arr):
+    
+    neighbors = []
+    neighbors_sum = 0
+    curr_vals = ((square-1) ** 2) - 1
+    prev_vals = len(previous_arr) #((square - 1) ** 2) - 1
+    prev_square = square - 2
+
+    period_square = int(math.floor(square / 2))
+
+    hinge_1 = square - 2
+    hinge_2 = 2 * square - 3
+    hinge_3 = 3 * square - 4
+    hinge_4 = 4 * square - 5
+    phinge_1 = prev_square - 2
+    phinge_2 = 2 * prev_square - 3
+    phinge_3 = 3 * prev_square - 4
+    phinge_4 = 4 * prev_square - 5
+    print current_arr
+    print previous_arr
+
+    print """
+square = %d
+period_square = %d
+steps = %d
+
+curr_vals = %d
+prev_vals = %d
+prev_square = %d
+
+hinge_1 = %d
+hinge_2 = %d
+hinge_3 = %d
+hinge_4 = %d
+phinge_1 = %d
+phinge_2 = %d
+phinge_3 = %d
+phinge_4 = %d    
+    """ % (
+        square,
+        period_square,
+        steps,
+
+        curr_vals,
+        prev_vals,
+        prev_square,
+
+        hinge_1,
+        hinge_2,
+        hinge_3,
+        hinge_4,
+        phinge_1,
+        phinge_2,
+        phinge_3,
+        phinge_4
+    )
+
+    # start
+    if steps == 0:
+        print '-- start'
+        neighbors.append("p{}={}".format(0, previous_arr[0]))
+        neighbors.append("p{}={}".format(prev_vals-1, previous_arr[prev_vals-1]))
+        neighbors_sum += previous_arr[0]
+        neighbors_sum += previous_arr[prev_vals-1]
+
+    # start + 1
+    elif steps == 1:
+        print '-- start+1'
+        neighbors.append("p{}={}".format(0, previous_arr[0]))
+        neighbors.append("p{}={}".format(1, previous_arr[1]))
+        neighbors.append("p{}={}".format(prev_vals-1, previous_arr[prev_vals-1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[0]
+        neighbors_sum += previous_arr[1]
+        neighbors_sum += previous_arr[prev_vals-1]
+        neighbors_sum += current_arr[steps-1]
+
+    # upper right
+    elif steps == hinge_1:
+        print '-- UR'
+        prev_hinge = phinge_1
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += current_arr[steps-1]
+
+    # upper left
+    elif steps == hinge_2:
+        print '-- UL'
+        prev_hinge = phinge_2
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += current_arr[steps-1]
+
+    # lower left
+    elif steps == hinge_3:
+        print '-- LL'
+        prev_hinge = phinge_3
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += current_arr[steps-1]
+
+    # lower right - end, special!
+    elif steps == hinge_4:
+        print '-- LR-END'
+        prev_hinge = phinge_4
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors.append("{}={}".format(0, current_arr[0]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += current_arr[steps-1]
+        neighbors_sum += current_arr[0]
+
+    # lower right - end-1, also special!
+    elif steps == hinge_4-1:
+        print '-- LR-PENULTIMATE'
+        prev_hinge = phinge_4
+        neighbors.append("p{}={}".format(prev_vals-1, previous_arr[prev_vals-1]))
+        neighbors.append("p{}={}".format(prev_vals-2, previous_arr[prev_vals-2]))
+        neighbors.append("{}={}".format(0, current_arr[0]))
+        neighbors_sum += current_arr[0]
+        neighbors_sum += current_arr[steps-1]
+        neighbors_sum += previous_arr[prev_vals-1]
+        neighbors_sum += previous_arr[prev_vals-2]
+
+    # upper_right + 1
+    elif steps == hinge_1+1:
+        print '-- UR+1'
+        prev_hinge = phinge_1
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("p{}={}".format(prev_hinge+1, previous_arr[prev_hinge+1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors.append("{}={}".format(steps - 2, current_arr[steps - 2]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += previous_arr[prev_hinge+1]
+        neighbors_sum += current_arr[steps-1]
+        neighbors_sum += current_arr[steps-2]
+
+    # upper_left + 1
+    elif steps == hinge_2+1:
+        print '-- UL+1'
+        prev_hinge = phinge_2
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("p{}={}".format(prev_hinge+1, previous_arr[prev_hinge+1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors.append("{}={}".format(steps - 2, current_arr[steps - 2]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += previous_arr[prev_hinge+1]
+        neighbors_sum += current_arr[steps-1]
+        neighbors_sum += current_arr[steps-2]
+
+    # lower_left + 1
+    elif steps == hinge_3+1:
+        print '-- LL+1'
+        prev_hinge = phinge_3
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("p{}={}".format(prev_hinge+1, previous_arr[prev_hinge+1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors.append("{}={}".format(steps - 2, current_arr[steps - 2]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += previous_arr[prev_hinge+1]
+        neighbors_sum += current_arr[steps-1]
+        neighbors_sum += current_arr[steps-2]
+
+    # upper_right - 1
+    elif steps == hinge_1-1:
+        print '-- UR-1'
+        prev_hinge = phinge_1
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("p{}={}".format(prev_hinge-1, previous_arr[prev_hinge-1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += previous_arr[prev_hinge-1]
+        neighbors_sum += current_arr[steps-1]
+
+    # upper_left - 1
+    elif steps == hinge_2-1:
+        print '-- UL-1'
+        prev_hinge = phinge_2
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("p{}={}".format(prev_hinge-1, previous_arr[prev_hinge-1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += previous_arr[prev_hinge-1]
+        neighbors_sum += current_arr[steps-1]
+
+    # lower_left - 1
+    elif steps == hinge_3-1:
+        print '-- LL-1'
+        prev_hinge = phinge_3
+        neighbors.append("p{}={}".format(prev_hinge, previous_arr[prev_hinge]))
+        neighbors.append("p{}={}".format(prev_hinge-1, previous_arr[prev_hinge-1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_hinge]
+        neighbors_sum += previous_arr[prev_hinge-1]
+        neighbors_sum += current_arr[steps-1]
+
+    # ELSE calculate three inner members for middle members
+    else:
+        print '-- MIDDLE'
+        if steps > hinge_3:
+            hinge = hinge_4
+            phinge = phinge_4
+        elif steps > hinge_2:
+            hinge = hinge_3
+            phinge = phinge_3
+        elif steps > hinge_1:
+            hinge = hinge_2
+            phinge = phinge_2
+        elif steps > 0:
+            hinge = hinge_1
+            phinge = phinge_1
+        diff = hinge - steps
+        prev_diff = diff - 1
+        prev_spot = phinge - prev_diff
+
+        print "hinge: {}".format(hinge)
+        print "phinge: {}".format(phinge)
+        print "current back from hinge: {}".format(diff)
+        print "previous back from hinge: {}".format(prev_diff)
+        print "previous index back from hinge: {}".format(prev_spot)
+
+        neighbors.append("p{}={}".format(prev_spot+1, previous_arr[prev_spot+1]))
+        neighbors.append("p{}={}".format(prev_spot, previous_arr[prev_spot]))
+        neighbors.append("p{}={}".format(prev_spot-1, previous_arr[prev_spot-1]))
+        neighbors.append("{}={}".format(steps - 1, current_arr[steps - 1]))
+        neighbors_sum += previous_arr[prev_spot+1]
+        neighbors_sum += previous_arr[prev_spot]
+        neighbors_sum += previous_arr[prev_spot-1]
+        neighbors_sum += current_arr[steps-1]
+
+    return [neighbors, neighbors_sum]
+
+
+prev_square_arr = [1, 2, 4, 5, 10, 11, 23, 25]
+curr_square_arr = []
+
+current_value = 0
+current_steps = 0
+total_steps = 0
+square_size = 5
+
+while total_steps < 1000000:
+
+    print "-- **"
+    [neighbors,nsum] = calculate_neighbors(square_size, current_steps, prev_square_arr, curr_square_arr)
+    print neighbors
+    print nsum
+    curr_square_arr.append(nsum)
+    current_value = nsum
+    max_len = 4 * square_size - 5
+
+    print "{} > {}".format(len(curr_square_arr),max_len)
+
+    if len(curr_square_arr) > max_len:
+        print "RESET!!!!!!"
+        prev_square_arr = curr_square_arr
+        curr_square_arr = []
+        square_size += 2
+        current_steps = -1
+
+    current_steps += 1
+    total_steps += 1
+    print current_value
+    print "-- *****\n\n"
+
+    if current_value > 368078:
+        print '!*!*!*!* {} *!*!*!*!*'.format(current_value)
+        break
+
+# correct answer: 369601, in the 9 square
 
 
